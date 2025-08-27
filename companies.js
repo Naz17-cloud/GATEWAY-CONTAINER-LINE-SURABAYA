9ede3fbc2a24229aba7fa95c3d61e9a3e6eb9f34
Naz17-cloud <mnasrul220@gmail.com>
2025-08-27 23:37:52 +0700

    Create companies.js

---
+++ companies.js
@@ -0,0 +1,8 @@
+// Fallback jika JSON tidak berhasil dimuat
+window.COMPANIES = {
+  "SUNGGONG LOGISTICS": ["Joko", "Widodo"],
+  "FORIN LOGISTIK": ["Prabowo", "Subiantoro"],
+  "PUMA SUB": ["Intans", "Segara"],
+  "LINK PACIFIC INDONUSA": ["NANAS"]
+  "LOGWIN AIR AND OCEAN INDONESIA": ["NUNIS"]
+};
