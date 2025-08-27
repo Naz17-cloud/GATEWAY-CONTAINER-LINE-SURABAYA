8b008687c06da77ce45cd4ec207fdff0c3d9825d
Naz17-cloud <mnasrul220@gmail.com>
2025-08-27 23:49:49 +0700

    Delete companies.js

--- companies.js
+++
@@ -1,8 +0,0 @@
-// Fallback jika JSON tidak berhasil dimuat
-window.COMPANIES = {
-  "SUNGGONG LOGISTICS": ["Joko", "Widodo"],
-  "FORIN LOGISTIK": ["Prabowo", "Subiantoro"],
-  "PUMA SUB": ["Intans", "Segara"],
-  "LINK PACIFIC INDONUSA": ["NANAS"]
-  "LOGWIN AIR AND OCEAN INDONESIA": ["NUNIS"]
-};
